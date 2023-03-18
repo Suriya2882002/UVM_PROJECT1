7b99f9c3b09cb8001ef12eac15b1b398728808c3
Suriya2882002 <112601117+Suriya2882002@users.noreply.github.com>
2023-03-18 17:22:45 +0530

    Add files via upload

---
+++ UVM PROJECT/UVM_PROJECT_1/spi_protocol/ut/uvm_tb/src/tb.sv
@@ -0,0 +1,35 @@
+`include "uvm_macros.svh"
+ import uvm_pkg::*;
+ `include "../../../rtl/top.sv"
+ `include "spi_i.sv"
+ `include "test.sv"
+
+  module tb;
+  
+  
+  spi_i vif();
+  
+  top dut (.wr(vif.wr), .clk(vif.clk), .rst(vif.rst), .addr(vif.addr), .din(vif.din), .dout(vif.dout), .done(vif.done), .err(vif.err));
+  
+  initial begin
+    vif.clk <= 0;
+  end
+
+  always #10 vif.clk <= ~vif.clk;
+
+  
+ 
+  
+  initial begin
+    uvm_config_db#(virtual spi_i)::set(null, "*", "vif", vif);
+    run_test("test");
+   end
+  
+  
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars;
+  end
+
+  
+endmodule
